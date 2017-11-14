2a39e83ae011d9b7c138105fc3a96e5f4df9c697
Anas Khan <anas.khan96@outlook.com>
2017-11-14 18:16:39 +0530

    converted timestamp to time and plotting stationary time series

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from datetime import datetime
 
-data = pd.read_csv('bitcoin_data.csv')
-input('Data read completed. Press enter key to continue: ')
+data = pd.read_csv('bitcoin_data_time.csv')
 data = data.dropna()
-data['Time'] = data['Timestamp'].apply(lambda x: pd.to_datetime(datetime.fromtimestamp(x)))
 data = data.set_index(data['Time'])
-plt.plot(data[['Close', 'Time']])
 
 # checking stationarity of the time series data
+plt.plot(data['Close'])
+plt.show()
