cc91e11e40dbadd3f79b26a5b56d2d25a195caa1
Anas Khan <anas.khan96@outlook.com>
2017-11-16 23:00:00 +0530

    some stuff

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import matplotlib.pyplot as plt
+from matplotlib.pylab import rcParams
 import pandas as pd
 from datetime import datetime
+rcParams['figure.figsize'] = 15, 6
 
 data = pd.read_csv('bitcoin_data_time.csv')
 data = data.dropna()
-data = data.set_index(data['Time'])
+data = data.set_index(data['Timestamp'])
+data.index = data.index.to_datetime()
+data.index = pd.to_datetime(data.index, format='%Y')
+data = data.resample('44000Min')
+print("Length of data =", len(data))
+close_values = data['Close']
 
 # checking stationarity of the time series data
-plt.plot(data['Close'])
+original = plt.plot(close_values, color='blue', label='Original')
+rolmean = pd.rolling_mean(close_values, window=12)
+mean = plt.plot(rolmean, color='red', label='Moving Average')
+plt.legend(loc='best')
+plt.title('Moving Average vs Original values over time')
 plt.show()
