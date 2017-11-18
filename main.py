792a718301cdf56caae086fa6363b2a4fe8a6686
Anas Khan <anas.khan96@outlook.com>
2017-11-18 12:46:10 +0530

    formatting the right way

--- main.py
+++ main.py
@@ -12,18 +12,15 @@ test_data = data.tail(500)
 data = data.set_index(['Date'])
 data.index = data.index.to_datetime()
 alpha_vals = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]
-mse = []
 
-# checking stationarity of the time series data
 for alpha in alpha_vals:
     plt.plot(data['Close'], color='blue', label='Original')
     ses = pd.ewma(data['Close'], com = 1/alpha-1)
     ses_test = ses.tail(500)
-    mse.append(sum([(x - y)**2 for x, y in zip(ses_test, test_data['Close'])])/500)
-    print("Predicted value for next day: ", (ses.tail(1) + alpha * (data.tail(1)['Close'] - ses.tail(1))))
+    mse = sum([(x - y)**2 for x, y in zip(ses_test, test_data['Close'])])/500
+    print("Mean squared error for alpha as {0} = {1}".format(alpha,mse))
     plt.plot(ses, color='red', label='Exponential Smoothing')
     plt.legend(loc='best')
     plt.title('Exponential Smoothing with alpha = {0} vs Original values over time'.format(alpha))
     plt.show()
-
-print(mse)
+    print("Predicted value for next day: ", (ses.tail(1) + alpha * (data.tail(1)['Close'] - ses.tail(1))))
